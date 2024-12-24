ddbfbb3bb0ca151293cd1cc9a8bac74ca04bae71
Ekaputripertiwi <2200016133@webmail.uad.ac.id>
2024-12-24 19:56:24 +0700

    eka_p8

---
+++ app.integration.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('./app'); // Pastikan path-nya sesuai dengan lokasi file app.js Anda
+
+describe('GET /', () => {
+    it('should respond with Hello, World!', async () => {
+        const response = await request(app).get('/');
+        expect(response.status).toBe(200);
+        expect(response.text).toBe('Hello, World!');
+    });
+});
